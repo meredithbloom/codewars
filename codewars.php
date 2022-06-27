b6a348dafd76631ecbb6ee9d6cdf3364758553f6
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-06-27 15:38:15 -0400

    did simple php problem

--- codewars.php
+++ codewars.php
@@ -1,3 +1,24 @@
+<!--  square every digit -->
+
+function square_digits($num){
+    $array = str_split($num);
+    $squared = '';
+    foreach ($array as $num) {
+    $squared = $squared.($num**2);
+    }
+    return $squared;
+}
+
+
+square_digits(9119)
+square_digits(24680)
+square_digits(13579)
+square_digits(0)
+
+
+
+
+<!--  -->
 function solution($number){
 $sum = 0;
 for ($count = 3; $count <= $number; $count++) { if ($count % 3===0 && $count % 5===0) { $sum+=0; } elseif ($count % 3===0) { $sum+=$count; } elseif ($count % 5===0) { $sum+=$count; } else { $sum+=0; } } return $sum; }
