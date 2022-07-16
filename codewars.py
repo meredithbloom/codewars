f2ba7e69663d6ad266c7609b515507a39f691f7a
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-07-16 17:28:54 -0400

    added python 5 kyu

--- codewars.py
+++ codewars.py
@@ -1,3 +1,21 @@
+# Where my anagrams at? 
+# write a function that will find all the anagrams of a word from a list. two inputs - a word and an array of words. return an array of all the anagrams in the list or an empty array if there are none
+from collections import Counter
+def anagrams(word, words):
+    counter = Counter(word)
+    anagrams = []
+    print(counter)
+    for w in words:
+        if len(w) == len(word):
+            c = Counter(w)
+            if len(counter-c) == 0:
+                anagrams.append(w)
+    print(anagrams)        
+            
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])
+
 # RGB to Hex conversion
 def rgb(r,g,b):
     def convert(decimal):
@@ -19,11 +37,11 @@ def rgb(r,g,b):
     print(rgb)
 
 
-rgb(0, 0, 0)
-rgb(1, 2, 3)
-rgb(255, 255, 255)
-rgb(254, 253, 252)
-rgb(-20, 275, 125)
+# rgb(0, 0, 0)
+# rgb(1, 2, 3)
+# rgb(255, 255, 255)
+# rgb(254, 253, 252)
+# rgb(-20, 275, 125)
 
 
 # Directions Reduction
