0e45804a1f26b0165da429cb1f55c39d415b0bbc
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-07-27 14:05:19 -0400

    sorted files into folders by language. added javascript solution that was way harder than expected

--- javascript/codewars.js
+++ javascript/codewars.js
@@ -1,3 +1,26 @@
+// Where my anagrams at? 5kyu
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none.
+
+
+function anagrams(word, words) {
+    let anagrams = []
+    let newWords = words.filter(w => w.length === word.length)
+    let original = word.split('').sort().join('')
+    for (let w of words) {
+        let temp = w.split('').sort().join('')
+        if (original === temp) {
+            anagrams.push(w)
+        }
+    }
+    console.log(anagrams)
+    return anagrams
+}
+
+
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])
+anagrams('laser', ['lazing', 'lazy', 'lacer'])
+
 // multiple of 3 or 5
 
 function solution(number) {
