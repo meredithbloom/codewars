dbadbc204b30bfc6c17665216544f66be4b928c8
Meredith Bloom <31168207+meredithbloom@users.noreply.github.com>
2022-08-03 19:50:25 +0000

    added partial solution to python

--- python/codewars2.py
+++ python/codewars2.py
@@ -1,3 +1,23 @@
+# Tic Tac Toe checker - 5kyu
+# If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
+
+# Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is an "O", like so:
+'''
+[[0, 0, 1],
+ [0, 1, 2],
+ [2, 1, 0]]
+'''
+# Function should return -1 if board is unfinished AND no one has won yet (if there are empty spaces), 1 if 'X' won, 2 if 'O' won, or 0 if it's a draw
+
+def is_solved(board):
+	
+	pass
+
+
+
+
+
+
 # Scramblies - 5kyu
 # Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
 # Only lower case letters will be used (a-z). No punctuation or digits will be included. Performance needs to be considered.
