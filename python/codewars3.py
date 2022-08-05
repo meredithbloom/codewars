a4a9dfddf1d6849d7c1f10555501828b59baef48
Meredith Bloom <31168207+meredithbloom@users.noreply.github.com>
2022-08-05 00:02:29 +0000

    added solution for 4kyu python problem

--- python/codewars3.py
+++ python/codewars3.py
@@ -1,11 +1,13 @@
 # Permutations - 4kyu
 # In this kata you have to create all permutations of a non empty input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
 # Order of permutations doesn't matter
-from itertools import combinations
+import itertools
 
 def permutations(s):
-	combos = combinations(s, len(s))
-	print(combos)
+	print(s)
+	combos = set(itertools.permutations(s))
+	perms = ["".join(x) for x in combos]
+	print(perms)
 
 
 permutations('a')
