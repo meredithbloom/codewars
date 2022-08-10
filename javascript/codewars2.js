fa8761f839dea4b0ceac13a62e75a2df576bbb86
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-08-10 12:09:06 -0400

    partial solution to product of consec fib numbers javascript

--- javascript/codewars2.js
+++ javascript/codewars2.js
@@ -1,3 +1,34 @@
+// Product of consecutive Fib numbers - 5 kyu
+/*
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+such as
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+[F(n), F(n+1), true]
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prod, you will return
+[F(n), F(n+1), false]
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+*/
+
+function productFib(prod) {
+	let fibs = [0, 1, 1, 2];
+	if (prod > 2) {
+		
+	}
+
+
+}
+
+
+
+
 // Calculating with Functions - 5 kyu
 // This time we want to write calculations using functions and get the results. Let's have a look at some examples:
 /*
