3f70a92463864cd71fc9dbb86d0b9d70e2361fe0
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-08-12 16:43:17 -0400

    added another php solution

--- php/codewars.php
+++ php/codewars.php
@@ -1,5 +1,30 @@
 
 <?php
+// Create Phone Number - 6 kyu
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+// createPhoneNumber([1,2,3,4,5,6,7,8,9,0]); 
+// => returns "(123) 456-7890"
+
+// Don't forget the space after the closing parentheses!
+function createPhoneNumber($numbersArray) {
+    $area = implode("",array_slice($numbersArray, 0, 3));
+    $middle = implode("",array_slice($numbersArray, 3, 3));
+    $last = implode("",array_slice($numbersArray, 6, 4));
+    echo "({$area}) {$middle}-{$last}"."\n";
+    return "({$area}) {$middle}-{$last}";
+
+}
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+// createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+
+// alternative solution using regex
+function createPhoneNumber2(array $digits): string
+{
+    return sprintf("(%d%d%d) %d%d%d-%d%d%d%d", ...$digits);
+}
+
+
 // Replace with Alphabet Position - 6kyu
 // In this kata you are required to, given a string, replace every letter with its position in the alphabet.
 //If anything in the text isn't a letter, ignore it and don't return it.
