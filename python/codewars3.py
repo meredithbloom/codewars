ca0f23c030b61b856d7553f03bcc1ad3cdc1b01d
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-09-15 16:26:45 -0400

    using python more itertools package to solve python 4 kyu problem... doesnt work in codewars so i need to use groupby

--- python/codewars3.py
+++ python/codewars3.py
@@ -1,3 +1,37 @@
+# Range Extraction - 4 kyu
+# A format for expressing an ordered list of integers is to use a comma separated list of either: 
+# individual integers,
+# or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17"
+# Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+
+import more_itertools as more
+from itertools import groupby
+
+solution1 = [-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]
+
+def solution(args):
+    final = ''
+    for group in more.consecutive_groups(args):
+        g = list(group)
+        #print(g)
+        if len(g) > 2:
+            reduced = str(g[0])+'-'+str(g[-1])
+            print(reduced)
+            final += reduced+', '
+            print(final)
+        else:
+            reduced = str(g[0])
+            print(reduced)
+            final += reduced+', '
+    print(final.strip(', '))
+    return final.strip(', ')
+
+
+solution(solution1)
+
+# needs to return string in correct format
+
+
 # Snail - 4 kyu
 # Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
 array1 = [[1, 2, 3],
@@ -85,7 +119,7 @@ def snail(snail_map):
 
 #snail(array2)
 
-snail(array5)
+#snail(array5)
 
 # Best Travel - 5 kyu
 
