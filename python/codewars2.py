08f00a958764e7376b052bf0ad89fb4bc1d4d8a5
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-10-03 18:00:43 -0400

    solution passed basic tests

--- python/codewars2.py
+++ python/codewars2.py
@@ -17,23 +17,62 @@ import re
 
 def increment_string(string):
     pattern1 = r"[0-9](?=[0-9]|$)"
-    num_split = re.findall(pattern1, string)
+    pattern2 = r"[0-9](?=\D)"
+    # check if there are numbers within string (not at end)
+    bad_nums = re.findall(pattern2, string)
+    # if there are, we need to only look at portion of string AFTER those mid-string nums
+    if len(bad_nums) > 0:
+        bad_num = re.search(pattern2, string)
+        bad_index = bad_num.span()
+        sliced_string = string[bad_index[1]:]
+        num_split = re.findall(pattern1, sliced_string)
+    else:
+        # if there are no bad nums, we can just look at whole string
+        num_split = re.findall(pattern1, string)
+    if len(num_split) == 0:
+        # if string has no nums at the end, we just add 1
+        new_string = string+'1'
+        print(new_string)
+        return new_string
+    else:
+        joined = int("".join(num_split))
+        #print(num_split)
+        new_num = joined + 1
+        #print(new_num)
+        if len(str(new_num)) < len(num_split):
+            # if we need leading zeros
+            zero_count = len(str(new_num))
+            new_string = string[:-zero_count]+str(new_num)
+            print(new_string)
+            return new_string
+        elif len(str(new_num)) > len(num_split):
+            # if we carry over a zero, i.e. 99 -> 100
+            start_ind = len(num_split)
+            new_string = string[:-start_ind]+str(new_num)
+            print(new_string)
+            return new_string
+        elif len(str(new_num)) == len(num_split):
+            start_ind = len(num_split)
+            new_string = string[:-start_ind]+str(new_num)
+            print(new_string)
+            return new_string
     
-    # first we need to find where the number starts
-    #num_split.append(string[-1])
-    print(num_split)
+    
+    
+        
     #print(re.split(pattern, string))
 
 
-#increment_string("foo")
+increment_string("foo")
 increment_string("foobar123")
 increment_string("foo0042")
-#increment_string("foo9")
+increment_string("foo9")
 increment_string("foobar23")
 increment_string("foo099")
 increment_string("foobar00")
 increment_string("foobar99")
 increment_string("fo99obar99")
+increment_string("")
 
 # The first non-repeating character - 5 kyu
 
