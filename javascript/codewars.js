c34eed0cdd087467b758a3322ab8b24127cd4a65
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-10-07 16:55:33 -0400

    added solution to simple python 6 kyu

--- javascript/codewars.js
+++ javascript/codewars.js
@@ -1,3 +1,6 @@
+
+
+
 // Pete, the baker - 5 kyu
 
 // Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
