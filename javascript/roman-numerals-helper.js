10efcb6768f0490a83b32b34bf0454e7a232d0c6
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-10-10 16:04:26 -0400

    partial solution to roman numeral helper javascript

--- javascript/roman-numerals-helper.js
+++ javascript/roman-numerals-helper.js
@@ -7,27 +7,117 @@
 // input range - 1<= n < 4000
 
 
-class RomanNumerals {
-    
+class RomanNumeralsHelper {
     romanValues = {
         'I': 1,
         'IV': 4,
         'V': 5,
+        'IX': 9,
         'X': 10,
+        'XL': 40,
         'L': 50,
+        'XC': 90,
         'C': 100,
+        'CD': 400,
         'D': 500,
+        'CM': 900,
         'M': 1000
     }
-    
     numeralValues = {
         1: 'I',
         4: 'IV',
         5: 'V',
+        9: 'IX',
         10: 'X',
+        40: 'XL',
         50: 'L',
+        90: 'XC',
         100: 'C',
+        400: 'CD',
         500: 'D',
+        900: 'CM',
         1000: 'M'
     }
+
+    checkThousands(num) {
+        // number of whole thousands, then remainder
+        let thousands = Math.floor(num / 1000)
+        let remHundreds = num - thousands*1000
+        return {thousands, remHundreds}
+    }
+
+    checkHundreds(num) {
+        // number of whole hundreds
+        let hundreds = Math.floor(num / 100)
+        let remTens = num-hundreds*100
+        return {hundreds, remTens}
+    }
+
+    checkTens(num) {
+        // number of whole tens
+        let tens = Math.floor(num / 10)
+        let remFives = num-tens*10
+        return {tens, remFives}
+    }
+
+    checkFives(num) {
+        let fives = Math.floor(num / 5)
+        let ones = num - fives * 5
+        return {fives, ones}
+    }
+
+    checkIfRoman(num) {
+        if (this.numeralValues[num]) {
+            return this.numeralValues[num]
+        }
+    }
+
+    toRoman(num) {
+        // if number is defined in base dictionary
+        if (this.numeralValues[num]) {
+            return this.numeralValues[num]
+        }
+        // check for thousands, hundreds, tens, ones
+        else {
+            let romanNumeral = ''
+            let { thousands, remHundreds } = this.checkThousands(num)
+            let { hundreds, remTens } = this.checkHundreds(remHundreds)
+            let { tens, remFives } = this.checkTens(remTens)
+            let {fives, ones} = this.checkFives(remFives)
+            console.log(`${thousands}000 + ${hundreds}00 + ${tens}0 + ${fives*5} + ${ones}`)
+            let tally = [thousands*1000, hundreds*100, tens*10, fives*5, ones]
+            for (let val of tally) {
+                if (this.checkIfRoman(val)) {
+                    //console.log(this.checkIfRoman(val))
+                    romanNumeral += this.checkIfRoman(val)
+                } else {
+                    // calculate what roman numeral should be
+                    console.log(val)
+                }
+            }
+            console.log(romanNumeral)
+        }
+    }
+
+    fromRoman(romanNum) {
+        // if roman numeral is defined in base dictionary
+        if (this.romanValues[romanNum]) {
+            return this.romanValues[romanNum]
+        }
+        // we know there is at least 1000
+        else if (romanNum.includes('M')) {
+
+        }
+    }
+
 }
+
+const RomanNumerals = new RomanNumeralsHelper()
+//console.log(RomanNumerals.toRoman(1000))
+//console.log(RomanNumerals.fromRoman('M'))
+
+console.log(RomanNumerals.toRoman(1990))
+console.log(RomanNumerals.toRoman(2008))
+// RomanNumerals.fromRoman('XXI')
+// RomanNumerals.fromRoman('IV')
+// RomanNumerals.fromRoman('MMVIII')
