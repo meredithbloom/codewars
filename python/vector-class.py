65f14a12c239a6923206938dd5183feaab91c818
Meredith Bloom <meredithbloom@merediths-mbp.lan>
2023-05-31 18:21:18 -0400

    progress on python problem vector class

--- python/vector-class.py
+++ python/vector-class.py
@@ -36,7 +36,26 @@ class Vector:
         if self.length != len(other_vector):
             raise ValueError('vectors are not of equal length')
         else:
+            new_vector = []
             for i in range(0, self.length-1):
                 new = self.vals[i] + other_vector[i]
+                new_vector.append(new)
+            return self.__init__(new_vector)
 
+    def subtract(self, other_vector):
+        if self.length != len(other_vector):
+            raise ValueError('vectors are not of equal length')
+        else:
+            continue
 
+    def dot(self, other_vector):
+        if self.length != len(other_vector):
+            raise ValueError('vectors are not of equal length')
+        else:
+            continue
+
+    def norm(self):
+        total = 0
+        for x in range(0, self.length-1):
+            total += self.vals[0]**2
+        return total
